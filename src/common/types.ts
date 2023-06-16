c6e589ddb938e2bf5b28ea2e3242414e25cfd122
Eason Huang <yuxuanh@amazon.com>
2023-06-15 17:51:39 -0700

    Stub interface
    Add stub functions for
    1. The AuthHelper interface containing following functions
      1. getMapAuthenticationOptions
      2. getLocationClientConfig
      3. getCredentials
    2. The `withIdentityPool` to instantiate AuthHelper from Cognito Identity Pool Id.

---
+++ src/common/types.ts
@@ -0,0 +1,28 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+import { AwsCredentialIdentity } from "@aws-sdk/types";
+
+// eslint-disable-next-line  @typescript-eslint/no-empty-interface
+export interface MapAuthenticationOptions {}
+
+// eslint-disable-next-line  @typescript-eslint/no-empty-interface
+export interface LocationClientConfig {}
+
+export interface getMapAuthenticationOptionsFunc {
+  (): MapAuthenticationOptions;
+}
+
+export interface getLocationClientConfigFunc {
+  (): LocationClientConfig;
+}
+
+export interface getCredentialsFunc {
+  (): AwsCredentialIdentity;
+}
+
+export interface AuthHelper {
+  getMapAuthenticationOptions: getMapAuthenticationOptionsFunc;
+  getLocationClientConfig: getLocationClientConfigFunc;
+  getCredentials: getCredentialsFunc;
+}
