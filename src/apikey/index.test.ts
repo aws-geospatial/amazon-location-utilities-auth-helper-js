b07066ad7ae8027cc2af306ad39b82dff2c65cac
Eason Huang <yuxuanh@amazon.com>
2023-06-20 14:47:09 -0700

    Fix the names in the APIKey test suite.

--- src/apikey/index.test.ts
+++ src/apikey/index.test.ts
@@ -3,15 +3,15 @@
 
 import { withAPIKey } from "./index";
 
-describe("AuthHelper for Cognito", () => {
+describe("AuthHelper for APIKey", () => {
+  it("should provide stub getLocationClientConfig function", async () => {
+    const authHelper = await withAPIKey("api.key");
+    expect(authHelper.getLocationClientConfig()).toStrictEqual({});
+  });
+
   it("should not provide stub getMapAuthenticationOptions function", async () => {
     const authHelper = await withAPIKey("api.key");
     expect("getMapAuthenticationOptions" in authHelper).toBe(false);
-  });
-
-  it("should not provide stub getLocationClientConfig function", async () => {
-    const authHelper = await withAPIKey("api.key");
-    expect(authHelper.getLocationClientConfig()).toStrictEqual({});
   });
 
   it("should not provide getCredentials function", async () => {
