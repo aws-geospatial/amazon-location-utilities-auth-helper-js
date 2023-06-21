b7a72cca0c7d4dee9c6ecb13a192b36188fa9dfd
Eason Huang <121910016+eashuang@users.noreply.github.com>
2023-06-21 16:53:50 -0700

    Add APIKey helper implementation (#7)
    * Add APIKey helper implementation
    
    1. Add APIKey helper implementation
    2. Separate the interface for the helper functions majorly for maps and sdk.

--- src/apikey/index.ts
+++ src/apikey/index.ts
@@ -4,13 +4,24 @@
 import { SDKAuthHelper } from "../common/types";
 
 /**
- * Creates an auth helper instance using APIKey.
+ * Creates an auth helper instance using APIKey. Its `getLocationClientConfig` function creates a signer to set the
+ * APIKey in all the commands of a client.
  *
  * @param apiKey APIKey
  */
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export async function withAPIKey(apiKey: string): Promise<SDKAuthHelper> {
   return {
-    getLocationClientConfig: () => ({}),
+    getLocationClientConfig: () => ({
+      signer: {
+        sign: async (requestToSign) => {
+          // APIKey in the command can override the APIKey set by auth helper.
+          requestToSign.query = {
+            key: apiKey,
+            ...(requestToSign.query ?? {}),
+          };
+          return requestToSign;
+        },
+      },
+    }),
   };
 }
